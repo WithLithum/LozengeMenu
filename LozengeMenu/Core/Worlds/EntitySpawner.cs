3f9950aec3fe08d416c38805824df3d24241b60e
WithLithum <WithLithum@foxmail.com>
2022-12-16 16:39:12 +0800

    🐛 Fix spawn vehicle not cancellable

--- LozengeMenu/Core/Worlds/EntitySpawner.cs
+++ LozengeMenu/Core/Worlds/EntitySpawner.cs
@@ -18,6 +18,13 @@ internal static class EntitySpawner
         {
             var input = Game.GetUserInput(WindowTitle.EnterMessage20, string.Empty, 20);
 
+            if (input?.Length == 0)
+            {
+                // User cancelled
+                // We cannot detect if user actually cancelled or just gave us an empty string
+                return;
+            }
+
             var hash = Natives.GetHashKey(input);
 
             if (!Natives.IsModelValid(hash) || !Natives.IsModelAVehicle(hash))
