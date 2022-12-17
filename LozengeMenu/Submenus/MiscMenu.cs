4e9df056a8ca2743583bfdaf8db90458c7ca4fa8
WithLithum <WithLithum@foxmail.com>
2022-12-17 14:43:25 +0800

    ✨ Pause vehicle and ped population

--- LozengeMenu/Submenus/MiscMenu.cs
+++ LozengeMenu/Submenus/MiscMenu.cs
@@ -12,6 +12,8 @@ public class MiscMenu : ISubMenu
     private readonly NativeMenu _menu = new("Lozenge", "Misc Options");
     private readonly NativeCheckboxItem _blackOut = new("Blackout Mode");
     private readonly NativeCheckboxItem _blackOutVehicles = new("Blackout Vehicles", true);
+    private readonly NativeCheckboxItem _itemPauseVehicle = new("Pause Vehicle Population");
+    private readonly NativeCheckboxItem _itemPausePeds = new("Pause Ped Population");
     private readonly NativeCheckboxItem _riotMode = new("Riot Mode");
 
     public NativeMenu Create()
@@ -20,12 +22,26 @@ public class MiscMenu : ISubMenu
         _blackOutVehicles.CheckboxChanged += BlackoutVehiclesChanged;
         _blackOutVehicles.Enabled = false;
         _riotMode.CheckboxChanged += RiotChanged;
+        _itemPauseVehicle.CheckboxChanged += PauseVehicleChanged;
+        _itemPausePeds.CheckboxChanged += PausePedsChanged;
 
         _menu.Add(_blackOut);
         _menu.Add(_blackOutVehicles);
+        _menu.Add(_itemPauseVehicle);
+        _menu.Add(_itemPausePeds);
         _menu.Add(_riotMode);
 
         return _menu;
+    }
+
+    private void PausePedsChanged(object sender, EventArgs e)
+    {
+        Natives.SetPedPopulationBudget(_itemPausePeds.Checked ? 0 : 3);
+    }
+
+    private void PauseVehicleChanged(object sender, EventArgs e)
+    {
+        Natives.SetVehiclePopulationBudget(_itemPauseVehicle.Checked ? 0 : 3);
     }
 
     private void RiotChanged(object sender, EventArgs e)
